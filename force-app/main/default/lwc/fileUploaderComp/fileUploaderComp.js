c419b42e4e4bcdf58de99f788470345c1db7b246
ouharri <ouharrioutman@gmail.com>
2023-07-21 18:04:34 +0100

    adding lws

---
+++ force-app/main/default/lwc/fileUploaderComp/fileUploaderComp.js
@@ -0,0 +1,38 @@
+import { LightningElement, api } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import uploadFile from '@salesforce/apex/FileUploaderClass.uploadFile'
+export default class FileUploaderCompLwc extends LightningElement {
+    @api recordId;
+    fileData
+    openfileUpload(event) {
+        const file = event.target.files[0]
+        var reader = new FileReader()
+        reader.onload = () => {
+            var base64 = reader.result.split(',')[1]
+            this.fileData = {
+                'filename': file.name,
+                'base64': base64,
+                'recordId': this.recordId
+            }
+            console.log(this.fileData)
+        }
+        reader.readAsDataURL(file)
+    }
+    
+    handleClick(){
+        const {base64, filename, recordId} = this.fileData
+        uploadFile({ base64, filename, recordId }).then(result=>{
+            this.fileData = null
+            let title = `${filename} uploaded successfully!!`
+            this.toast(title)
+        })
+    }
+
+    toast(title){
+        const toastEvent = new ShowToastEvent({
+            title, 
+            variant:"success"
+        })
+        this.dispatchEvent(toastEvent)
+    }
+}
