8d2e44e19fb181fee87dee867172a1604450296d
ouharri <ouharrioutman@gmail.com>
2023-07-24 01:20:57 +0100

    commit

--- force-app/main/default/lwc/quizeLwc/quizeLwc.js
+++ force-app/main/default/lwc/quizeLwc/quizeLwc.js
@@ -1,4 +1,9 @@
 import { LightningElement } from 'lwc';
 export default class QuizeLwc extends LightningElement {
-
+    connectedCallback() {
+        const urlParams = new URLSearchParams(window.location.search);
+        const token = urlParams.get('token');
+        console.log('token', token);
+        // Faites quelque chose avec le token ici
+    }
 }
