24810807581301ece4849028fd059838d3fe9e36
ouharri <ouharrioutman@gmail.com>
2023-07-25 16:30:17 +0100

    add quizzes apex logic

--- force-app/main/default/lwc/quizeLwc/quizeLwc.js
+++ force-app/main/default/lwc/quizeLwc/quizeLwc.js
@@ -1,10 +1,11 @@
-import { LightningElement, wire, track } from 'lwc';
+import { LightningElement, wire, track, api } from 'lwc';
 import { CurrentPageReference } from 'lightning/navigation';
 import flowbitejs from '@salesforce/resourceUrl/flowbitejs';
 import flowbitecss from '@salesforce/resourceUrl/flowbitecss';
 import validateToken from '@salesforce/apex/TokenManager.validateToken';
-import quizePatternImage from '@salesforce/resourceUrl/quizePatternImage';
+import getIdsFromToken from '@salesforce/apex/TokenManager.getIdsFromToken';
 import { loadStyle, loadScript } from 'lightning/platformResourceLoader';
+import getQuizzesWithQuestionsAndOptions from '@salesforce/apex/QuizController.getQuizzesWithQuestionsAndOptions'; import quizePatternImage from '@salesforce/resourceUrl/quizePatternImage';
 
 export default class QuizeLwc extends LightningElement {
 
@@ -12,15 +13,20 @@ export default class QuizeLwc extends LightningElement {
     @track countDown = 30;
     @track imageBgLink;
 
+    @api quiz;
+
     @wire(CurrentPageReference)
     getStateParameters(currentPageReference) {
         if (currentPageReference) {
-            const token = currentPageReference.state?.token;
-            this.validateToken(token);
+            this.token = currentPageReference.state?.token;
+            this.validateToken(currentPageReference.state?.token);
         }
     };
 
+    token = null;
     timer = null;
+    quizId = null;
+    LeadId = null;
 
     get timerClass() {
         return this.countDown < 10 ? 'warning' : 'count-down';
@@ -38,11 +44,11 @@ export default class QuizeLwc extends LightningElement {
     async validateToken(token) {
         if (token) {
             await validateToken({ encryptedToken: token })
-                .then(result => {
+                .then((result) => {
                     if (result) {
                         this.handleValidToken();
                     } else {
-                        this.handleInvalidToken();
+                        this.handleInvalidToken(token);
                     }
                 })
                 .catch(error => {
@@ -53,12 +59,16 @@ export default class QuizeLwc extends LightningElement {
         }
     }
 
-    startQuiz() {
-        this.isQuizStarted = true;
-    }
-
-    handleValidToken() {
-        console.log('Token valide !');
+    async handleValidToken() {
+        console.log('Token valide ! ');
+        await getIdsFromToken({ encryptedToken: this.token })
+            .then(result => {
+                this.quizId = result.QuizId;
+                this.LeadId = result.LeadId;
+            })
+            .catch(error => {
+                console.error('Erreur lors de la récupération des IDs :', error);
+            });
     }
 
     handleInvalidToken() {
@@ -81,14 +91,28 @@ export default class QuizeLwc extends LightningElement {
         }
     }
 
+    startQuiz() {
+        this.isQuizStarted = true;
+        this.countDownTimer();
+    }
+
+    async getQuizzes() {
+        await getQuizzesWithQuestionsAndOptions({ quizId: this.quizId })
+            .then(result => {
+                this.quizzes = result;
+                console.log('Quizzes : ', this.quizzes);
+            })
+            .catch(error => {
+                console.error('Erreur lors de la récupération des quizzes :', error);
+            });
+    }
+
     connectedCallback() {
         this.imageBgLink = `background-image: linear-gradient(
             to left top,
             rgb(255, 255, 255, 0.7),
             rgb(252, 252, 252, 0.7)
           ),url('${quizePatternImage}')`;
-        this.countDownTimer();
     }
-
 
 }
